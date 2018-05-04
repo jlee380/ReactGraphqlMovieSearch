759b9a3cf8b57336fae2fb3cdc1d1e784a4512c3
jlee380 <jonghunjhl@gmail.com>
2018-05-04 09:55:10 -0400

    MovieList added
    fetching movie data  via graphql

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,15 +2,34 @@
 // We'll replace this component with one that uses GraphQL to fetch movies
 
 import React from "react";
-
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import {Component} from "react";
+import Movie from "./Movie";
+import { Item } from "semantic-ui-react";
 
 class MovieList extends Component {
-
   render() {
+    const { data } = this.props;
+    if (data.loading) return <div>Loading...</div>;
+    if (data.error) return <div>Error!!</div>;
+    if (data.movies.length === 0) return <div>No Movies</div>;
 
     return (
-      <div></div>
+      <Item.Group divided>
+        {data.movies.map(movie => (
+          <Movie
+            key={movie.movieId}
+            title={movie.title}
+            poster={movie.poster}
+            plot={movie.plot}
+            rating={movie.imdbRating}
+            genres={movie.genres}
+            similar={movie.similar}
+            year={movie.year}
+          />
+        ))}
+      </Item.Group>
     );
 
   }
@@ -18,4 +37,25 @@ class MovieList extends Component {
   
 };
 
-export default MovieList;
+const MovieListWithData = graphql(
+  gql`
+    query MovieListQuery($title: String!) {
+      movies: movies(subString: $title, limit: 10) {
+        title
+        movieId
+        imdbRating
+        plot
+        poster
+        year
+        genres
+        similar {
+          movieId
+          poster
+          title
+        }
+      }
+    }
+  `
+)(MovieList)
+
+export default MovieListWithData;
